41dec5d2669eb3c8d9057d21aafb0e5dbae9de4b
olga <shkvyryaos@gmail.com>
2022-09-01 20:47:18 +0500

    changes method furst task

--- HWork3/Program.cs
+++ HWork3/Program.cs
@@ -1,13 +1,14 @@
-﻿//Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
-/*
-void Dist(double x1, double y1, double z1, double x2, double y2, double z2)
+﻿/*
+//Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+
+double Dist(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double dx=x2-x1;
     double dy=y2-y1;
     double dz=z2-z1;
     double dist=Math.Round(Math.Sqrt(Math.Pow(dx, 2) +Math.Pow(dy, 2)+Math.Pow(dz, 2)),2);
-    Console.WriteLine($"Расстояние между точками А и В равно {dist}");
-}
+    return dist;
+   }
 
 Console.WriteLine("Введите координаты точки А:");
 Console.Write("x1=");
@@ -25,10 +26,10 @@ double y2=Convert.ToDouble(Console.ReadLine());
 Console.Write("z2=");
 double z2=Convert.ToDouble(Console.ReadLine());
 Console.WriteLine($"Введены координаты точки B:{x2};{y2};{z2};");
-
-Dist(x1,y1,z1,x2,y2,z2);
+Console.WriteLine($"Расстояние между точками А и В равно {Dist(x1,y1,z1,x2,y2,z2)}");
 
 */
+
 /*
 // Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 void Cube(int Num)
